59ff41e5fb16134e145d1d7dfe01dee1e4bd4ffc
Yağızcan Arslan <yagizcan.arslan93@gmail.com>
2020-05-18 23:40:48 +0300

    Client update

---
+++ src/Client.java
@@ -0,0 +1,15 @@
+public class Client  {
+    private Phone phone;
+
+    Client(Phone phone) {
+        this.phone = phone;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+}
